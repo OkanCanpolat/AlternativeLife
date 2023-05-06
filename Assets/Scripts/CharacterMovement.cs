0231468c663f3112af52b10903d379d3645b0b5b
Okan Canpolat <okancanpolat002344@hotmail.com>
2023-05-06 13:24:20 +0300

    character movement, cinemachine

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float movementSpeed;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+    }
+
+
+}
